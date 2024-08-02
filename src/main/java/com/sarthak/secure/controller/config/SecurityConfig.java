376f5ae6f3775118cd7f2f0150da8bd48ad03383
sarthakjana09 <sarthakjana09@gmail.com>
2024-08-02 23:26:19 +0530

    Basic auth implemneted

---
+++ src/main/java/com/sarthak/secure/controller/config/SecurityConfig.java
@@ -0,0 +1,29 @@
+package com.sarthak.secure.controller.config;
+
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFIlterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests((requests) -> requests
+                .requestMatchers("/actuator/**")
+                .permitAll()
+                .anyRequest()
+                .authenticated())
+                .httpBasic(Customizer.withDefaults());
+
+        return http.build();
+    }
+}
